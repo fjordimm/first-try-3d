ec1d5d3ff4fbe30f5bd9a17a1765677cf632e2a2
fjordimm <fjordimm@gmail.com>
2023-06-18 16:50:38 -0700

    made a 3d cube

--- src/FirstTry3D.cpp
+++ src/FirstTry3D.cpp
@@ -5,11 +5,13 @@
 #include <cmath>
 
 void printMatVisualization(bool** mat, int height, int width);
+void drawLine(bool** mat, int height, int width, int x1, int y1, int x2, int y2);
+void drawLine3d(bool** mat, int height, int width, double x1, double y1, double z1, double x2, double y2, double z2);
 
 int main(void)
 {
-	const int height = 280;
-	const int width = 650;
+	const int height = 150;
+	const int width = 300;
 
 	bool** mat = (bool**) malloc(height * sizeof(bool*));
 	for (int h = 0; h < height; h++)
@@ -25,7 +27,7 @@ int main(void)
 
 			// Testing; a diagonal line
 			/*
-			if (h == w)
+			if (h < w)
 			{
 				mat[h][w] = true;
 			}
@@ -33,15 +35,56 @@ int main(void)
 		}
 	}
 
-	mat[104][102] = true;
+	drawLine3d(mat, height, width, 50, 50, 300, 50, 100, 300);
+	drawLine3d(mat, height, width, 50, 100, 300, 100, 100, 300);
+	drawLine3d(mat, height, width, 100, 100, 300, 100, 50, 300);
+	drawLine3d(mat, height, width, 100, 50, 300, 50, 50, 300);
+
+	drawLine3d(mat, height, width, 50, 50, 300, 50, 50, 350);
+	drawLine3d(mat, height, width, 50, 100, 300, 50, 100, 350);
+	drawLine3d(mat, height, width, 100, 50, 300, 100, 50, 350);
+	drawLine3d(mat, height, width, 100, 100, 300, 100, 100, 350);
+
+	drawLine3d(mat, height, width, 50, 50, 350, 50, 100, 350);
+	drawLine3d(mat, height, width, 50, 100, 350, 100, 100, 350);
+	drawLine3d(mat, height, width, 100, 100, 350, 100, 50, 350);
+	drawLine3d(mat, height, width, 100, 50, 350, 50, 50, 350);
 
 	printMatVisualization(mat, height, width);
+}
+
+void drawLine3d(bool** mat, int height, int width, double x1, double y1, double z1, double x2, double y2, double z2)
+{
+	z1 /= width;
+	z2 /= width;
+
+	int x1p = (int)(x1 / z1);
+	int y1p = (int)(y1 / z1);
+	
+	int x2p = (int)(x2 / z2);
+	int y2p = (int)(y2 / z2);
+
+	drawLine(mat, height, width, x1p, height - y1p, x2p, height - y2p);
+}
+
+void drawLine(bool** mat, int height, int width, int x1, int y1, int x2, int y2)
+{
+	int intervals = std::max(height, width);
+	
+	for (int i = 0; i < intervals; i++)
+	{
+		int x = x1 + ((x2 - x1) * i) / intervals;
+		int y = y1 + ((y2 - y1) * i) / intervals;
+
+		if (x > 0 && x < width && y > 0 && y < height)
+		{ mat[y][x] = true; }
+	}
 }
 
 void printMatVisualization(bool** mat, int height, int width)
 {
 	const double widthRescaleFactor = 0.47;
-	const double scaleFactor = 6.9420;
+	const double scaleFactor = 3.69420;
 
 	const double hScaleFactor = scaleFactor;
 	const double wScaleFactor = scaleFactor * widthRescaleFactor;
